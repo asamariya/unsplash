d164c1455eef6c1abef55fd87fc5937c61250f1e
Abhishek Samariya <asamariya@gmail.com>
2019-11-19 13:30:50 +1100

    Search and add unsplash images on the site
    - Display Image description, author and image dimensions

--- search.js
+++ search.js
@@ -1,19 +1,49 @@
 const formTag = document.querySelector("form");
 const inputTag = formTag.querySelector("input");
+const resultsTag = document.querySelector("section.results");
 
 const apiKey =
   "c7575ba5219bc16dc1f7157fd62fb192226d60625f96a1bc6b44f52e35e80693";
 
-const searchUrl = `https://api.unsplash.com/search/photos?per_page=24&query=blue`;
+const searchUrl = `https://api.unsplash.com/search/photos?per_page=24&query=`;
 
 const searchUnsplash = async term => {
-  let response = await fetch(searchUrl, {
+  let response = await fetch(searchUrl + term, {
     headers: {
       Authorization: `Client-ID ${apiKey}`
     }
   });
   let data = await response.json();
-  console.log(data);
+
+  //format unsplash's results to suit our needs
+  return data.results.map(result => {
+    return {
+      imageSrc: result.urls.regular,
+      description: result.description || "Untitled",
+      author: result.user.name,
+      width: result.width,
+      height: result.height,
+      backgroundColor: (result.color || "#eee") + "33"
+    };
+  });
+};
+
+// Add results to the page
+const addResults = results => {
+  resultsTag.innerHTML = "";
+  results.forEach(result => {
+    resultsTag.insertAdjacentHTML(
+      "beforeend",
+
+      `<div class="single-result">
+        <div class="image" style="background-color: ${result.backgroundColor}">
+          <img src="${result.imageSrc}">
+        </div>
+        <h2>${result.description}</h2>
+        <p>By ${result.author} &mdash; ${result.width} x ${result.height}</p>
+      </div>`
+    );
+  });
 };
 
 // When we submit the form, get the info from input
@@ -21,5 +51,7 @@ formTag.addEventListener("submit", e => {
   e.preventDefault();
   const searchTerm = inputTag.value;
 
-  searchUnsplash(searchTerm);
+  searchUnsplash(searchTerm).then(results => {
+    addResults(results);
+  });
 });
